83b20c24957c63a90bc2094877953999ee329e9f
Elf80lvl <49438937+Elf80lvl@users.noreply.github.com>
2025-08-06 16:12:56 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "91a9ad296bcfd45d8035791bca1106b3",
+const RESOURCES = {"flutter_bootstrap.js": "c40505e6f8d1610c1e6b75d0c888cd18",
 "version.json": "4e4f8f63d65d325553c79462ed511b8a",
 "index.html": "083928cb10b8fc906374923a8d5c664c",
 "/": "083928cb10b8fc906374923a8d5c664c",
-"main.dart.js": "2fcb1619725ec5491eaaed879900b1d5",
+"main.dart.js": "dbc7a1c0587be9d6ee44aa536bca11d9",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/favicon.ico": "d0c0af3a99ada002caf2c75486099071",
